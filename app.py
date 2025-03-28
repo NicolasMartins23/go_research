cef8380a6cfc2550645dfef83763777bf1db95a7
Nicolas Martins <contato.sistemasmartins@gmail.com>
2025-03-28 15:58:48 -0300

    Upload: app.py
    Streamlit application core

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from gene_research import research
+
+def main() -> None:
+    """Main function to run the Streamlit app."""
+    st.title("GO Term Enrichment Analysis")
+
+    # Sidebar for menu selection
+    menu_options: dict[str] = {
+        "research": ":microscope: GO Research",
+    }
+
+    # Add the options to the sidebar
+    menu = st.sidebar.radio("Choose an option", (menu_options["research"]))
+
+    genes_list: list[str] = []
+
+    if menu == menu_options["research"]:
+        # User manually enters gene symbols
+        genes_input = st.sidebar.text_area("Enter gene symbols (comma-separated)", "")
+
+        # Show the submit button after input field
+        submit_button = st.sidebar.button("SUBMIT", type="primary")
+
+        if submit_button:
+            # Convert input genes to a list only when button is clicked
+            genes_list = [gene.strip() for gene in genes_input.split(",")]
+
+            if genes_list:
+                st.write("### Gene Symbols Entered", genes_list)
+
+                results = research(genes_list=genes_list)
+
+                # Show results in the main panel
+                st.write("### Enrichment Results", results)
+        else:
+            st.subheader("How to use?")
+            st.write('Type a gene standardize symbol and click "Subtmit" to start the Gene Ontology research')
+
+    spacer = st.sidebar.html("<hr>")
+    link = st.sidebar.button(":arrow_upper_right: Learn more about Gene Ontology", type="tertiary")
+
+if __name__ == "__main__":
+    main()
